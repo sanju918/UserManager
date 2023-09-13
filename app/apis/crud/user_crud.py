d93e014e718167d61c3bcddf346c173368dfa1ae
sanju918 <42485083+sanju918@users.noreply.github.com>
2023-09-13 22:57:20 +0530

    added user crud

--- app/apis/crud/user_crud.py
+++ app/apis/crud/user_crud.py
@@ -1,16 +1,62 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import update, insert, delete
+import bcrypt
 
 from app.db.models.user_model import User
+from app.db.db_config import get_async_db
 
-# from app.db.schemas.user_schema import (
-#     UserCreateSchema,
-#     UserUpdateSchema,
-#     UserPwdUpdateSchema,
-# )
+from app.db.schemas.user_schema import (
+    UserCreateSchema,
+    UserUpdateSchema,
+    UserPwdUpdateSchema,
+)
 
 
 async def get_user(db: AsyncSession, user_id: int):
     query = select(User).where(User.id == user_id)
     result = await db.execute(query)
     return result.scalar_one_or_none()
+
+
+async def get_users(db: AsyncSession):
+    query = select(User).execution_options(populate_existing=True)
+    result = await db.execute(query)
+    return result.scalars().all()
+
+
+async def create_user(db: AsyncSession, user: UserCreateSchema):
+    hashed_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
+    db_user = User(
+        email=user.email,
+        role=user.role,
+        is_active=user.is_active,
+        password=hashed_password.decode("utf-8"),
+    )
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+
+    return db_user
+
+
+# async def create_user(db: AsyncSession, user: UserCreateSchema):
+#     db_user = User(**user.dict())
+#     db.add(db_user)
+#     await db.commit()
+#     await db.refresh(db_user)
+#
+#     return db_user
+
+
+async def get_user_by_email(db: AsyncSession, email: str):
+    query = select(User).where(User.email == email)
+    result = await db.execute(query)
+    return result.scalar_one_or_none()
+
+
+# Function to verify a user's password
+def verify_password(plain_password, hashed_password):
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
--- app/apis/user_router.py
+++ app/apis/user_router.py
@@ -2,9 +2,12 @@ import fastapi
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from typing import List
+
 from ..db.db_config import get_async_db
-from .crud.user_crud import get_user
-from ..db.schemas.user_schema import UserSchema
+from ..db.schemas.user_schema import UserSchema, UserCreateSchema
+
+from .crud.user_crud import get_user, get_users, create_user, get_user_by_email
 
 router = fastapi.APIRouter()
 
@@ -16,4 +19,29 @@ async def read_user(user_id: int, async_db: AsyncSession = Depends(get_async_db)
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Requested user not found"
         )
+    return res
+
+
+@router.get("/users", response_model=List[UserSchema], tags=["Users"])
+async def read_users(async_db: AsyncSession = Depends(get_async_db)):
+    res = await get_users(db=async_db)
+    if not res:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Requested user not found"
+        )
+    return res
+
+
+@router.post("/users", response_model=UserSchema, tags=["Users"])
+async def create_new_user(
+    user: UserCreateSchema, async_db: AsyncSession = Depends(get_async_db)
+):
+    db_user = await get_user_by_email(db=async_db, email=user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Provided Email is not available to use.",
+        )
+    res = await create_user(db=async_db, user=user)
+
     return res
